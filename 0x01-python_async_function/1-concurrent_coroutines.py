dff4dfac6f16fb58fae55fc84b4c7845f986977c
swoji <davidookuku@gmail.com>
2023-08-07 17:04:30 +0300

    removed sort()

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,9 +9,16 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    '''Executes wait_random n times.
-    '''
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: wait_random(max_delay), range(n)))
-    )
-    return sorted(wait_times)
+    """Spawn wait_random n times"""
+    tasks = []
+    wait_times = []
+
+    for i in range(n):
+        task = wait_random(max_delay)
+        tasks.append(task)
+
+    for task in asyncio.as_completed((tasks)):
+        wait_time = await task
+        wait_times.append(wait_time)
+
+    return wait_times
